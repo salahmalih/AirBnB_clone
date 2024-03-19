80377bd091838794022220410c6b162b97e7549f
salahmalih <c130084552@taalim.ma>
2024-03-19 19:47:48 +0000

    first test of task 3

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+from datetime import datetime
+import models
+from uuid import uuid4
+
+
+from uuid import uuid4
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
+    def __init__(self):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        """Return the str representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
